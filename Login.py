243d24835a141664479e8f0f4a285aa194f8f6a8
manojingalgi <45203283+manojingalgi@users.noreply.github.com>
2024-05-21 20:24:35 +0530

    adding title

--- Login.py
+++ Login.py
@@ -10,6 +10,6 @@ driver.find_element(By.NAME,"email").send_keys("hello@gmail.com")
 driver.find_element(By.ID,"exampleInputPassword1").send_keys("123456")
 driver.find_element(By.CSS_SELECTOR,"input[name='name']").send_keys("Manoj")
 driver.find_element(By.ID,"exampleCheck1").click()
-
+print(driver.title)
 time.sleep(5)
 driver.quit()
